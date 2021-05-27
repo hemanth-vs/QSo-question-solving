4177f836d0014c3bf319e21e894cffcd2083b174
HemanthVarmaS <s.hemanthvarma@gmail.com>
2021-05-27 20:05:27 +0530

    add working java code for LUCKYFR-Lucky Four question

---
+++ Beginner/LUCKYFR.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class LUCKYFR {
+  public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    int t = Integer.parseInt(br.readLine());
+    for(int tIter = 0; tIter<t;tIter++) {
+      int num = Integer.parseInt(br.readLine());
+      int dup = num;
+      int count = 0;
+      while(dup!=0) {
+        if(dup%10 == 4) count++;
+        dup = dup/10;
+      }
+      System.out.println(count);
+    }
+    br.close();
+  }
+}
