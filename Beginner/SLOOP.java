91ef281a0b333b4a376a3e141c1e394831d9dc59
HemanthVarmaS <s.hemanthvarma@gmail.com>
2021-05-28 17:01:26 +0530

    add java code for SLOOP-Coldplay question

---
+++ Beginner/SLOOP.java
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/SLOOP
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SLOOP {
+  public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException{
+    int t = Integer.parseInt(br.readLine());
+    for(int tIter=0;tIter<t;tIter++) {
+      StringTokenizer sTokenizer = new StringTokenizer(br.readLine());
+      int m = Integer.parseInt(sTokenizer.nextToken());
+      int s = Integer.parseInt(sTokenizer.nextToken());
+      System.out.println(m/s);
+    }
+    br.close();
+  }
+}
