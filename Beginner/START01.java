789c52bee3bf2143f831ea16ab9fd9d935ed4b86
HemanthVarmaS <s.hemanthvarma@gmail.com>
2021-06-01 22:38:59 +0530

    add solution for START01 question

---
+++ Beginner/START01.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class START01 {
+  public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException{
+    System.out.println(Integer.parseInt(br.readLine()));
+    br.close();
+  }
+}
