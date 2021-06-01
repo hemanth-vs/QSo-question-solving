c70346798f5fe0660e8cce764e5b6bad559bd23c
HemanthVarmaS <s.hemanthvarma@gmail.com>
2021-06-01 22:32:11 +0530

    SOLBLTY question solved

---
+++ Beginner/SOLBLTY.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SOLBLTY {
+  public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException{
+    int t = Integer.parseInt(br.readLine());
+    for (int tIter = 0; tIter < t; tIter++) {
+      StringTokenizer sTokenizer = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(sTokenizer.nextToken());
+      int a = Integer.parseInt(sTokenizer.nextToken());
+      int b = Integer.parseInt(sTokenizer.nextToken());
+      int res = (a + (100-x)*b)*10;
+      System.out.println(res);
+    }
+    br.close();
+  }
+}
